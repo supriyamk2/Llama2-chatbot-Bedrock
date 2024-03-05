d53a7f8f343bde34c8fb9ab3013be59b8c63cb08
supriyamk2 <62048223+supriyamk2@users.noreply.github.com>
2024-03-06 04:05:57 +0530

    Update chatbot_backend.py

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -4,8 +4,6 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 
 def demo_chatbot(aws_access_key_id, aws_secret_access_key):
-    
-
     demo_llm = Bedrock(
         credentials_profile_name='default',  # Adjust profile name if needed
         model_id='meta.llama2-70b-chat-v1',
@@ -15,36 +13,22 @@ def demo_chatbot(aws_access_key_id, aws_secret_access_key):
             "max_gen_len": 512
         }, 
         client_kwargs={  
-           "aws_access_key_id": aws_access_key_id,
-           "aws_secret_access_key": aws_secret_access_key,
-       }
+            "aws_access_key_id": aws_access_key_id,
+            "aws_secret_access_key": aws_secret_access_key,
+        }
     )
     return demo_llm
 
-    #return demo_llm.invoke(input_text)
-
-
-#response = demo_chatbot("hi, what is your name?")
-#print(response)
-
-def demo_memory():
-    llm_data = demo_chatbot()
-    memory = ConversationBufferMemory(llm = llm_data, max_token_limit = 512)
+# Pass AWS credentials to demo_memory
+def demo_memory(aws_access_key_id, aws_secret_access_key):
+    llm_data = demo_chatbot(aws_access_key_id, aws_secret_access_key)
+    memory = ConversationBufferMemory(llm=llm_data, max_token_limit=512)
     return memory
 
-def demo_conversation(input_text, memory):
-    llm_chain_data = demo_chatbot()
+# Pass AWS credentials and memory to demo_conversation
+def demo_conversation(input_text, aws_access_key_id, aws_secret_access_key, memory):
+    llm_chain_data = demo_chatbot(aws_access_key_id, aws_secret_access_key)
     llm_conversation = ConversationChain(llm=llm_chain_data, memory=memory, verbose=True)
     
-    chat_reply = llm_conversation.predict(input = input_text)
+    chat_reply = llm_conversation.predict(input=input_text)
     return chat_reply
-    
-
-
-    
-
-
-
-
-
-
