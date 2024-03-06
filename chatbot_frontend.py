20644c5c018c188100cce7759b3c93c29bfc95c4
supriyamk2 <62048223+supriyamk2@users.noreply.github.com>
2024-03-06 06:00:29 +0530

    Update chatbot_frontend.py

--- chatbot_frontend.py
+++ chatbot_frontend.py
@@ -1,40 +1,36 @@
 import streamlit as st
+
 import chatbot_backend as demo
+
 
 st.title('Hi, This is LLama 2 model')
 
-# Load AWS credentials from Streamlit secrets
-aws_access_key_id = st.secrets["default"]["AWS_ACCESS_KEY_ID"]
-aws_secret_access_key = st.secrets["default"]["AWS_SECRET_ACCESS_KEY"]
-
-print(f"AWS Access Key ID: {aws_access_key_id}")
-print(f"AWS Secret Access Key: {aws_secret_access_key}")
-
-# Initialize demo_memory with AWS credentials
 if 'memory' not in st.session_state:
-    st.session_state.memory = demo.demo_memory(aws_access_key_id, aws_secret_access_key)
-
+    st.session_state.memory  =demo.demo_memory()
+    
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
-
+    
 for message in st.session_state.chat_history:
     with st.chat_message(message['role']):
         st.markdown(message['text'])
+        
 
 input_text = st.chat_input('Chat with Llama 2 model here')
+
 
 if input_text:
     with st.chat_message('user'):
         st.markdown(input_text)
         
-    st.session_state.chat_history.append({"role": "user", "text": input_text})
-    
-    # Use AWS credentials from Streamlit secrets in demo_conversation
-    chat_response = demo.demo_conversation(input_text=input_text, memory=st.session_state.memory,
-                                           aws_access_key_id=aws_access_key_id,
-                                           aws_secret_access_key=aws_secret_access_key)
+    st.session_state.chat_history.append({"role":"user",
+                                         "text":input_text})
+    chat_response = demo.demo_conversation(input_text=input_text, memory=st.session_state.memory)
     
     with st.chat_message("assistant"):
         st.markdown(chat_response)
+        
+    st.session_state.chat_history.append({"role":"assistant","text":chat_response})
+        
 
-    st.session_state.chat_history.append({"role": "assistant", "text": chat_response})
+    
