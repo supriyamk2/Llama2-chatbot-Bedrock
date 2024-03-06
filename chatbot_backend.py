5300e9635f0c9e2001eb2d57f3af211cd7661786
supriyamk2 <62048223+supriyamk2@users.noreply.github.com>
2024-03-06 06:00:13 +0530

    Update chatbot_backend.py

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -3,36 +3,41 @@ from langchain_community.llms import Bedrock
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 
-def demo_chatbot(aws_access_key_id, aws_secret_access_key):
-    client_kwargs = {
-        "aws_access_key_id": aws_access_key_id,
-        "aws_secret_access_key": aws_secret_access_key,
-    }
-
+def demo_chatbot():
     demo_llm = Bedrock(
         credentials_profile_name='default',
         model_id='meta.llama2-70b-chat-v1',
         model_kwargs={
-            "temperature": 0.9,
-            "top_p": 0.5,
-            "max_gen_len": 512
-        },
-        client_kwargs=client_kwargs  # Use the modified client_kwargs
-    )
-
+            
+            "temperature":0.9,
+            "top_p":0.5,
+            "max_gen_len":512})
     return demo_llm
 
+    #return demo_llm.invoke(input_text)
 
-# Pass AWS credentials to demo_memory
-def demo_memory(aws_access_key_id, aws_secret_access_key):
-    llm_data = demo_chatbot(aws_access_key_id, aws_secret_access_key)
-    memory = ConversationBufferMemory(llm=llm_data, max_token_limit=512)
+
+#response = demo_chatbot("hi, what is your name?")
+#print(response)
+
+def demo_memory():
+    llm_data = demo_chatbot()
+    memory = ConversationBufferMemory(llm = llm_data, max_token_limit = 512)
     return memory
 
-# Pass AWS credentials and memory to demo_conversation
-def demo_conversation(input_text, aws_access_key_id, aws_secret_access_key, memory):
-    llm_chain_data = demo_chatbot(aws_access_key_id, aws_secret_access_key)
+def demo_conversation(input_text, memory):
+    llm_chain_data = demo_chatbot()
     llm_conversation = ConversationChain(llm=llm_chain_data, memory=memory, verbose=True)
     
-    chat_reply = llm_conversation.predict(input=input_text)
+    chat_reply = llm_conversation.predict(input = input_text)
     return chat_reply
+    
+
+
+    
+
+
+
+
+
+
